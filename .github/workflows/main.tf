08a02010a092ec9c41743a632b09958b83e61ef4
Kalel <12826@intelizign.com>
2025-04-09 11:01:52 +0530

    Added main.tf

---
+++ .github/workflows/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-south-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-06b6e5225d1db5f46"  # Replace with latest Amazon Linux AMI for your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "GitHubActions-EC2"
+  }
+}
