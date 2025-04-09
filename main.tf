1c038ff1f67503d265105544adfef6aa48486d84
Kalel <12826@intelizign.com>
2025-04-09 17:43:40 +0530

    Added changes in main.tf

--- main.tf
+++ main.tf
@@ -2,19 +2,9 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# resource "aws_instance" "testec2" {
-#   ami           = "ami-06b6e5225d1db5f46"  # Replace with latest Amazon Linux AMI for your region
-#   instance_type = "t2.micro"
-
-#   tags = {
-#     Name = "GitHubActions-EC2"
-#   }
-# }
-
-# Craete VPC
+# Create VPC
 resource "aws_vpc" "testvpc" {
   cidr_block = "10.0.0.0/16"
-  
   tags = {
     Name = "TestVPC"
   }  
@@ -22,68 +12,69 @@ resource "aws_vpc" "testvpc" {
 
 # Create Subnet
 resource "aws_subnet" "testsubnet" {
-  vpc_id = aws_vpc.testvpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "ap-south-1"
+  vpc_id            = aws_vpc.testvpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "TestSubnet"
   }
 }
 
-# Create Internet gateway
+# Create Internet Gateway
 resource "aws_internet_gateway" "testigw" {
   vpc_id = aws_vpc.testvpc.id
 
   tags = {
     Name = "TestIGW"
   }
-  
 }
 
-# Create route table
+# Create Route Table
 resource "aws_route_table" "testrt" {
   vpc_id = aws_vpc.testvpc.id
 
-  route = {
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.testigw.id
+  }
 
-    tags = {
-      Name = "TestRouteTable"
-    }
+  tags = {
+    Name = "TestRouteTable"
   }
 }
 
-# Associate route table with subnet
+# Associate Route Table with Subnet
 resource "aws_route_table_association" "testassoc" {
-  subnet_id = aws_subnet.testsubnet.id
+  subnet_id      = aws_subnet.testsubnet.id
   route_table_id = aws_route_table.testrt.id
 }
 
-# Create a security group
+# Create Security Group
 resource "aws_security_group" "testsg" {
-  name = "test_sg"
+  name        = "test_sg"
   description = "Allow SSH and HTTP"
-  vpc_id = aws_vpc.testvpc.id
+  vpc_id      = aws_vpc.testvpc.id
 
-  ingress = {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress = {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp" 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress = {
-    from_port = 0
-    to_port = 0 
-    protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
+  egress {
+    from_port   = 0
+    to_port     = 0 
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -91,14 +82,13 @@ resource "aws_security_group" "testsg" {
   }
 }
 
-# Launch EC2 Instance in this above subnet and sg
+# Launch EC2 Instance
 resource "aws_instance" "testec2" {
-  ami = "ami-06b6e5225d1db5f46"
-  instance_instance_type = "t2.micro"
-  subnet_id = aws_subnet.testsubnet.id
-  vpc_security_group_ids = [aws_security_group.testsg.id]
-
-  associate_public_ip_address = true # So it gets a public IP
+  ami                         = "ami-06b6e5225d1db5f46"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.testsubnet.id
+  vpc_security_group_ids      = [aws_security_group.testsg.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "GitHubActions-EC2"
